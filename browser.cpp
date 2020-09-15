8b0e28d45e698e4bb277e277efdce76d4373f15a
Austin Hester <ahester@incomm.com>
2020-09-14 21:19:00 -0500

    actually show the images
    Signed-off-by: Austin Hester <ahester@incomm.com>

--- browser.cpp
+++ browser.cpp
@@ -14,7 +14,7 @@
 
 int parse_arguments(const int argc, const char** argv, std::string* input_dir_path, uint* rows, uint* cols);
 std::vector<std::string> open_dir(const char* dir_string);
-void open_dir(const char* dir_string, const std::string line_prefix, std::vector<std::string>* file_paths);
+void open_dir(const std::string dir_string, const std::string line_prefix, std::vector<std::string>* file_paths);
 int is_directory(const char* path);
 
 
@@ -26,21 +26,34 @@ main(int argc, const char** argv)
     std::string input_dir_path;
 
     int parse_result = parse_arguments(argc, argv, &input_dir_path, &rows, &cols);
-
     if (parse_result == 1) return 0;
     if (parse_result == -1) return -1;
 
-	std::cout << input_dir_path << std::endl;
+	std::cout << "Selected input directory:\t" << input_dir_path << std::endl;
 
     std::vector<std::string> file_paths = open_dir(input_dir_path.c_str());
+
     std::cout << std::endl << "Filename list:" << std::endl;
     for (std::vector<std::string>::iterator it = file_paths.begin(); it != file_paths.end(); ++it) {
-        std::cout << ' ' << *it << std::endl;
+        // std::cout << ' ' << *it << std::endl;
+        try {
+            cv::Mat image = cv::imread(*it);
+            if (image.empty()) {
+                std::cerr << "Cannot open input image: " + *it << std::endl;
+                continue;
+            }
+            cv::imshow(*it, image);
+            cv::waitKey(0);
+        } catch (std::string& str) {
+            std::cerr << "Error: " << *it << ": " << str << std::endl;
+        } catch (cv::Exception& e) {
+            std::cerr << "Error: " << *it << ": " << e.msg << std::endl;
+        }
     }
 	return 0;
 }
 
-
+// parse command line arguments
 int
 parse_arguments(int argc, const char** argv, std::string* input_dir_path, uint* rows, uint* cols)
 {
@@ -84,7 +97,7 @@ parse_arguments(int argc, const char** argv, std::string* input_dir_path, uint* 
     return 0;
 }
 
-
+// Wrapper for open_dir, returns vector of strings (filenames)
 std::vector<std::string>
 open_dir(const char* dir_string)
 {
@@ -93,13 +106,13 @@ open_dir(const char* dir_string)
     return file_paths;
 }
 
-
+// recursively open all directories and assemble a list of filenames
 void
-open_dir(const char* dir_string, const std::string line_prefix, std::vector<std::string>* file_paths)
+open_dir(const std::string dir_string, const std::string line_prefix, std::vector<std::string>* file_paths)
 {
     DIR *dr;
     try {
-        dr = opendir(dir_string); //open all directories
+        dr = opendir(dir_string.c_str()); //open all directories
     } catch (...) {
         std::cerr << "Failed to open directory: [" << dir_string << "]" << std::endl;
         return;
@@ -114,13 +127,14 @@ open_dir(const char* dir_string, const std::string line_prefix, std::vector<std:
         if (strcmp(drnt->d_name, ".") == 0 || strcmp(drnt->d_name, "..") == 0) {
             continue;
         }
+        // get the relative path
+        const std::string rel_path = dir_string + (dir_string.back() == '/' ? "" : std::string("/")) + std::string(drnt->d_name);
         // check if is a directory
-        const std::string rel_path = (dir_string + std::string("/") + std::string(drnt->d_name));
-        // std::cout << std::endl << rel_path <<std::endl << rel_path.c_str();
         is_dir = is_directory(rel_path.c_str());
-        std::cout << line_prefix << drnt->d_name << is_dir; //print all directory names
+        // print files in an organized manner
+        std::cout << line_prefix << drnt->d_name;
         if (is_dir) {
-            // open next directory, sorry for this line
+            // open next directory
             open_dir(rel_path.c_str(), line_prefix + "\t", file_paths);
         } else {
             (*file_paths).push_back(rel_path);
@@ -129,7 +143,7 @@ open_dir(const char* dir_string, const std::string line_prefix, std::vector<std:
     closedir(dr); //close the directory
 }
 
-
+// is the given path a directory?
 int
 is_directory(const char* path)
 {
